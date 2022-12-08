ffcbb90b15852b182d507a63f513032f88dbbd3f
luc0liv <luoliveiradacunha@gmail.com>
2022-12-08 17:13:31 -0300

    REQ 1: Configurando redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,14 @@
 // Coloque aqui suas actions
+const USER_ACTION = 'USER_ACTION';
+
+export const saveUserInfo = (user) => ({
+  type: USER_ACTION,
+  payload: user,
+});
+
+const WALLET_ACTION = 'WALLET_ACTION';
+
+export const saveWalletInfo = (wallet) => ({
+  type: WALLET_ACTION,
+  payload: wallet,
+});
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'USER_ACTION':
+    return {
+      ...state,
+      email: action.payload.email,
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
