566eec500734dff94c3eff9f378b9abd0adbbfe8
luc0liv <luoliveiradacunha@gmail.com>
2022-12-13 21:37:34 -0300

    REQ 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 class Table extends Component {
+  tableHeaders = ['Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
+    'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
+
   render() {
     return (
-      <div>Table</div>
+      <table>
+        {this.tableHeaders.map((header) => <th key={ header }>{header}</th>)}
+      </table>
     );
   }
 }
